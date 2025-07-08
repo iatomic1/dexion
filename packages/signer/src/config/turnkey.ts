1ededb11fee5a7e2d9746518166c149823630fd7
iatomic_1 <atomic.k.2739@gmail.com>
2025-07-08 19:18:10 +0100

    🗑️ Delete(signer): deleted config/turnkey.ts file because its not needed

--- packages/signer/src/config/turnkey.ts
+++
@@ -1,6 +0,0 @@
-export const turnkeyConfig = {
-	apiBaseUrl: "https://api.turnkey.com",
-	apiPrivateKey: process.env.TURNKEY_API_PRIVATE_KEY as string,
-	apiPublicKey: process.env.TURNKEY_API_PUBLIC_KEY as string,
-	defaultOrganizationId: process.env.NEXT_PUBLIC_ORGANIZATION_ID as string,
-};

34b3ecbe2453ed5a61d869d7bfdb2e5f4b59babf
iatomic_1 <atomic.k.2739@gmail.com>
2025-07-15 22:02:42 +0100

    ✨ Feat(web): added a theme switcher for users to switch between light/dark mode

---
+++ apps/web/src/components/layout/site-footer/theme-switcher.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { ThemeSwitcher } from "@repo/ui/components/ui/theme-switcher";
+import { useTheme } from "next-themes";
+
+const ThemeSwitcherTab = () => {
+	const { setTheme, theme } = useTheme();
+
+	const allowedThemes = ["light", "dark", "system"] as const;
+	const safeTheme = allowedThemes.includes(theme as any)
+		? (theme as "light" | "dark" | "system")
+		: "system";
+
+	return (
+		<ThemeSwitcher
+			defaultValue="system"
+			onChange={setTheme}
+			value={safeTheme}
+		/>
+	);
+};
+
+export default ThemeSwitcherTab;

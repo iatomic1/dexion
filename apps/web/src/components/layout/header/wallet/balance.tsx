24594b0066e4392ba9326bd295b861629c25c4f4
iatomic_1 <atomic.k.2739@gmail.com>
2025-07-16 14:44:28 +0100

    ✨ Feat(web): display STX balance in USD instead of plain amount

--- apps/web/src/components/layout/header/wallet/balance.tsx
+++ apps/web/src/components/layout/header/wallet/balance.tsx
@@ -24,14 +24,16 @@ import {
 import { useQuery } from "@tanstack/react-query";
 import { Copy } from "lucide-react";
 import type React from "react";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { useMediaQuery } from "~/app/(protected)/meme/[ca]/_components/trade-details";
+import { useBtcStxPriceContext } from "~/contexts/BtcStxPriceContext";
 import useCopyToClipboard from "~/hooks/useCopy";
 import { useSubscribeAddressTransactions } from "~/hooks/useSubscribeAddressTransactions";
 import { authClient } from "~/lib/auth-client";
 import { getBalance } from "~/lib/queries/hiro";
 import { formatTokenBalance } from "~/lib/utils/token";
 import Exchange from "./exchange";
+import Withdraw from "./withdraw";
 
 interface BalanceContentProps {
 	isPending: boolean;
@@ -56,17 +58,29 @@ function BalanceContent({
 		balanceData?.stx.balance as string,
 		6,
 	);
+	const {
+		prices,
+		isLoading: isPriceLoading,
+		isError,
+	} = useBtcStxPriceContext();
+	const stxPrice = useMemo(() => {
+		return prices?.find((p) => p.symbol === "stx");
+	}, [prices]);
 
 	const HeaderContent = () => (
 		<div className="flex justify-between flex-row">
 			<div className="flex gap-2 flex-col">
 				<span className="text-xs">Total Value</span>
-				{isPending || isLoading ? (
+				{isPending || isLoading || isPriceLoading ? (
 					<Skeleton className="h-7 w-24" />
 				) : isMobile ? (
-					<span className="text-lg font-semibold">${formattedBalance}</span>
+					<span className="text-lg font-semibold">
+						${((stxPrice?.current_price ?? 0) * formattedBalance).toFixed(2)}
+					</span>
 				) : (
-					<span className="text-lg font-semibold">${formattedBalance}</span>
+					<span className="text-lg font-semibold">
+						${((stxPrice?.current_price ?? 0) * formattedBalance).toFixed(2)}
+					</span>
 				)}
 			</div>
 			<div className="flex items-center gap-2">
@@ -111,16 +125,11 @@ function BalanceContent({
 					Deposit
 				</Button>
 			</Exchange>
-			<Exchange
-				mode="withdraw"
-				stxBalance={formattedBalance.toString()}
-				stxAddress={walletAddress}
-				onClose={onClose}
-			>
+			<Withdraw>
 				<Button className="rounded-full w-full" size="sm" variant={"default"}>
 					Withdraw
 				</Button>
-			</Exchange>
+			</Withdraw>
 		</div>
 	);
 

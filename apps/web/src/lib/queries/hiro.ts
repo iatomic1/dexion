24132b2f7e6094cdaaffade5639f50a65d26f918
iatomic_1 <atomic.k.2739@gmail.com>
2025-07-20 03:07:39 +0100

    🧑‍💻 Chore(web): setup hiro client for blockchain queries

--- apps/web/src/lib/queries/hiro.ts
+++ apps/web/src/lib/queries/hiro.ts
@@ -2,6 +2,7 @@ import type { AddressBalanceResponse } from "~/types/hiro/balance";
 import type { PaginatedFtBasicMetadataResponse } from "~/types/hiro/token";
 import type { Transaction } from "~/types/hiro/transactions";
 import makeFetch from "../helpers/fetch";
+import { client } from "../stacks-blockchain-api-client";
 
 export const getBalance = async (address: string) => {
 	try {
@@ -35,18 +36,46 @@ export const searchTokens = async (searchTerm: string) => {
 	}
 };
 
-export const getTransactions = async (address: string) => {
-	try {
-		return makeFetch<Transaction>(
-			"hiro",
-			`extended/v2/addresses/${address}/transactions`,
-			null,
-			{
-				method: "GET",
+export async function getTransactions(
+	address: string,
+	limit?: number,
+	offset?: number,
+) {
+	const query: Record<string, number> = {};
+	if (limit !== undefined) query.limit = limit;
+	if (offset !== undefined) query.offset = offset;
+
+	const { data } = await client.GET(
+		"/extended/v2/addresses/{address}/transactions",
+		{
+			params: {
+				path: { address },
+				query,
 			},
-		)();
-	} catch (error) {
-		console.error(error);
-		throw error;
-	}
-};
+		},
+	);
+
+	return data;
+}
+
+// export const getTransactions = async (
+// 	address: string,
+// 	limit?: number,
+// 	offset?: number,
+// ) => {
+// 	try {
+// 		const params = new URLSearchParams();
+// 		if (limit !== undefined) params.append("limit", String(limit));
+// 		if (offset !== undefined) params.append("offset", String(offset));
+
+// 		const query = params.toString();
+// 		const url = `extended/v2/addresses/${address}/transactions${query ? `?${query}` : ""}`;
+
+// 		return makeFetch<Transaction>("hiro", url, null, {
+// 			method: "GET",
+// 		})();
+// 	} catch (error) {
+// 		console.error(error);
+// 		throw error;
+// 	}
+// };

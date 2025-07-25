76a8f75130a12a3be848d5a148b4e57bd1a98fbb
iatomic_1 <atomic.k.2739@gmail.com>
2025-07-25 12:16:34 +0100

    ✨ Feat(web): add withAuth HOC and client-side session context provider

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -6,10 +6,10 @@ import "./globals.css";
 import { Toaster } from "@repo/ui/components/ui/sonner";
 import type { Metadata } from "next";
 import type React from "react";
-import { AuthProvider } from "~/components/providers/auth-providers";
 import { ClientQueryProvider } from "~/components/providers/query-client-provider";
 import { ThemeProvider } from "~/components/providers/theme-provider";
 import { turnkeyConfig } from "~/config/turnkey";
+import { AuthClientContextProvider } from "~/contexts/AuthClientContext";
 import { PresetsContextProvider } from "~/contexts/PresetsContext";
 import { DEV } from "~/lib/constants";
 import { geistMono, geistSans } from "./fonts/geist";
@@ -27,56 +27,56 @@ export default async function RootLayout({
 		<html lang="en" suppressHydrationWarning className="dark">
 			<body className={`${geistSans.variable} ${geistMono.variable}`}>
 				{/* <TokenRefresher /> */}
-				<ThemeProvider
-					attribute="class"
-					defaultTheme="dark"
-					enableSystem
-					disableTransitionOnChange
-				>
-					<Toaster
-						position="top-center"
-						// richColors
-						theme="light"
-						visibleToasts={1}
-						expand={true}
-					/>
-					{/* <WalletTrackerSocketProvider> */}
-					<PresetsContextProvider>
-						<div className="flex min-h-screen flex-col font-geist">
-							{/* {!DEV && (
+				<AuthClientContextProvider>
+					<ThemeProvider
+						attribute="class"
+						defaultTheme="dark"
+						enableSystem
+						disableTransitionOnChange
+					>
+						<Toaster
+							position="top-center"
+							// richColors
+							theme="light"
+							visibleToasts={1}
+							expand={true}
+						/>
+						{/* <WalletTrackerSocketProvider> */}
+						<PresetsContextProvider>
+							<div className="flex min-h-screen flex-col font-geist">
+								{/* {!DEV && (
 								<div className="flex w-full items-center justify-center text-center text-sm py-3 text-destructive">
 									This website is still actively in development.
 								</div>
 							)} */}
 
-							<ClientQueryProvider>
-								<main className="flex-1">
-									<AuthProvider>
+								<ClientQueryProvider>
+									<main className="flex-1">
 										<TurnkeyProvider config={turnkeyConfig}>
 											{children}
 										</TurnkeyProvider>
-									</AuthProvider>
-								</main>
-							</ClientQueryProvider>
-						</div>
-					</PresetsContextProvider>
-					{process.env.NODE_ENV === "production" && (
-						<Script
-							src="https://cdn.databuddy.cc/databuddy.js"
-							strategy="afterInteractive"
-							async
-							data-client-id="rAfsBCtxnJvNZkVLfnP3H"
-							data-track-attributes={false}
-							data-track-errors={true}
-							data-track-outgoing-links={false}
-							data-track-web-vitals={true}
-							data-track-sessions={false}
-							data-track-exit-intent="true"
-						/>
-					)}
+									</main>
+								</ClientQueryProvider>
+							</div>
+						</PresetsContextProvider>
+						{process.env.NODE_ENV === "production" && (
+							<Script
+								src="https://cdn.databuddy.cc/databuddy.js"
+								strategy="afterInteractive"
+								async
+								data-client-id="rAfsBCtxnJvNZkVLfnP3H"
+								data-track-attributes={false}
+								data-track-errors={true}
+								data-track-outgoing-links={false}
+								data-track-web-vitals={true}
+								data-track-sessions={false}
+								data-track-exit-intent="true"
+							/>
+						)}
 
-					{/* </WalletTrackerSocketProvider> */}
-				</ThemeProvider>
+						{/* </WalletTrackerSocketProvider> */}
+					</ThemeProvider>
+				</AuthClientContextProvider>
 			</body>
 		</html>
 	);

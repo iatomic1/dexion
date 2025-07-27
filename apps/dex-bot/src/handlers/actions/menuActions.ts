2f00d81e7d570890dd666eb89880fa71aa4f021e
iatomic_1 <atomic.k.2739@gmail.com>
2025-07-27 20:53:40 +0100

    ♻️ Refactor(dex-bot): remove duplicated action handler from menuActions.ts

--- apps/dex-bot/src/handlers/actions/menuActions.ts
+++ apps/dex-bot/src/handlers/actions/menuActions.ts
@@ -4,8 +4,5 @@ import * as messages from "@/messages";
 import type { DexBotContext } from "@/types/bot";
 
 export function registerMenuActions(bot: Telegraf<DexBotContext>) {
-	bot.action("add_wallet", (ctx) => {
-		ctx.answerCbQuery();
-		ctx.reply(messages.ADD_WALLET_PROMPT);
-	});
+	// Add actions here
 }

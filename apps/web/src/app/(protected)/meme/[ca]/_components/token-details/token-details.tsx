259d9f6bda23b4195747c5167ee2a324d1c3e87d
iatomic_1 <atomic.k.2739@gmail.com>
2025-07-27 14:56:52 +0100

    🐛 Fix(web): use correct useIsMobile

--- apps/web/src/app/(protected)/meme/[ca]/_components/token-details/token-details.tsx
+++ apps/web/src/app/(protected)/meme/[ca]/_components/token-details/token-details.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import type { TokenMetadata } from "@repo/tokens/types";
-import { useIsMobile } from "@repo/ui/hooks/use-mobile";
+import { useIsMobile } from "@repo/ui/hooks/use-is-mobile";
 import {
 	memo,
 	Suspense,

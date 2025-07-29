c64c6e54fe9bc1d9a6dce00b36bec01abfef5182
iatomic_1 <atomic.k.2739@gmail.com>
2025-07-29 21:56:37 +0100

    ⚗️ Chore(web): still experimenting

--- apps/dex-bot/src/lib/auth-client.ts
+++ apps/dex-bot/src/lib/auth-client.ts
@@ -1,11 +1,13 @@
+import { DOMAIN_NAME, FRONTEND_URL } from "@repo/shared-constants/constants.ts";
 import { createAuthClient } from "better-auth/client";
 import { oneTimeTokenClient } from "better-auth/client/plugins";
 
 export const authClient: any = createAuthClient({
-	baseURL:
-		process.env.NODE_ENV === "production"
-			? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-			: "http://localhost:3001",
+	// baseURL:
+	// 	process.env.NODE_ENV === "production"
+	// 		? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+	// 		: "http://localhost:3001",
+	baseURL: `https://beta.${DOMAIN_NAME}`,
 	plugins: [oneTimeTokenClient()],
 });
 

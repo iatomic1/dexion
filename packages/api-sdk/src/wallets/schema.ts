626132b118a4145a5e62c5d8c1557ef1c96831a6
iatomic_1 <atomic.k.2739@gmail.com>
2025-07-29 17:31:46 +0100

    🦺 Fix(api-sdk): improve emoji validation to account for multi-codepoint emojis

--- packages/api-sdk/src/wallets/schema.ts
+++ packages/api-sdk/src/wallets/schema.ts
@@ -1,7 +1,15 @@
 import z from "zod";
 
 export const addWalletSchema = z.object({
-	emoji: z.string().emoji().min(1).max(2),
+	emoji: z
+		.string()
+		.emoji()
+		.refine(
+			(value) => /^(\p{Emoji_Presentation}|\p{Emoji}\uFE0F)$/u.test(value),
+			{
+				message: "Invalid emoji. Please provide a single valid emoji.",
+			},
+		),
 	nickname: z.string().min(1).max(50),
 	walletAddress: z.string().min(1),
 });

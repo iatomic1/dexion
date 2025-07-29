fe7f9221a913409a5232c9c06ac95ce1bef10553
iatomic_1 <atomic.k.2739@gmail.com>
2025-07-29 23:10:37 +0100

    ⚰️ Chore(web): remove exposed env vars from UI in production builds

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -46,9 +46,6 @@ export default function Home() {
 				>
 					TradingView
 				</a>
-				<span>
-					{process.env.NODE_ENV} {process.env.NEXT_PUBLIC_VERCEL_URL}
-				</span>
 			</div>
 			<header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
 				<div className="container flex h-16 items-center justify-between">

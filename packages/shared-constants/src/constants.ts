2d190075e17bf72083801c8df3fc63b1b4c9b8fa
iatomic_1 <atomic.k.2739@gmail.com>
2025-08-01 11:33:26 +0100

    🩹 Fix(web): include www. in DOMAIN_NAME

--- packages/shared-constants/src/constants.ts
+++ packages/shared-constants/src/constants.ts
@@ -7,7 +7,7 @@ export const API_BASE_URL =
 	// "http://localhost:8080/api/v1/";
 	"https://dexion-backend-production-af99.up.railway.app/api/v1/";
 
-export const DOMAIN_NAME = "dexion.pro";
+export const DOMAIN_NAME = "www.dexion.pro";
 export const PUBLIC_BASE_URL = DEV
 	? "http://localhost:3001"
 	: `https://${DOMAIN_NAME}`;

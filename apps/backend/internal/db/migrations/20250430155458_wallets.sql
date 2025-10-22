d65ba212379691870ba0fde3bd5ab483245f037c
Atomic <atomic.k.2739@gmail.com>
2025-10-22 20:46:04 +0100

    ♻️ Refactor(backend): consolidate db migrations and update alert queries

--- apps/backend/internal/db/migrations/20250430155458_wallets.sql
+++ apps/backend/internal/db/migrations/20250430155458_wallets.sql
@@ -1,14 +1,14 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS wallets (
-  address TEXT NOT NULL UNIQUE, -- tags:`binding:"required" example:"SP1Y5YSTAHZ88XYK1VPDH24GY0HPX5J4JECTMY4A1"
+  address TEXT NOT NULL UNIQUE, -- tags:`binding:"required" example:"SP1Y5YSTAHZ88XYK1VPDH24GY0HPX5J4JECTMY4A1"`
   created_at TIMESTAMPTZ DEFAULT now()
 );
 
 CREATE TABLE IF NOT EXISTS user_wallets (
   user_id TEXT REFERENCES users(id),
-  wallet_address TEXT REFERENCES wallets(address) NOT NULL, -- tags:`binding:"required" example:"SP1Y5YSTAHZ88XYK1VPDH24GY0HPX5J4JECTMY4A1"
-  nickname TEXT NOT NULL, -- tags:`binding:"required" example:"iatomic"
+  wallet_address TEXT REFERENCES wallets(address) NOT NULL, -- tags:`binding:"required" example:"SP1Y5YSTAHZ88XYK1VPDH24GY0HPX5J4JECTMY4A1"`
+  nickname TEXT NOT NULL, -- tags:`binding:"required" example:"iatomic"`
   emoji TEXT,
   notifications BOOLEAN DEFAULT true NOT NULL,
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
@@ -16,11 +16,12 @@ CREATE TABLE IF NOT EXISTS user_wallets (
   PRIMARY KEY (user_id, wallet_address)
 );
 
-CREATE INDEX idx_user_wallets_custom ON user_wallets(user_id, wallet_address);
+CREATE INDEX IF NOT EXISTS idx_user_wallets_custom ON user_wallets(user_id, wallet_address);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 DROP TABLE IF EXISTS "wallets";
 DROP TABLE IF EXISTS "user_wallets";
+DROP INDEX IF EXISTS idx_user_wallets_custom;
 -- +goose StatementEnd

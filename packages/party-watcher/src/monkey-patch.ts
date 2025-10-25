618f8679ec262fc03505968baeba4884a2621652
Atomic <atomic.k.2739@gmail.com>
2025-10-25 14:07:58 +0100

    🗑️ Chore(party-watcher): remove dead and unused party-watcher package

--- packages/party-watcher/src/monkey-patch.ts
+++
@@ -1,8 +0,0 @@
-// packages/party-watcher/src/monkey-patch.ts
-
-// Monkey-patch the global fetch to remove the unsupported `cache` option.
-const originalFetch = global.fetch;
-global.fetch = (input, init) => {
-  const { cache, ...rest } = init || {};
-  return originalFetch(input, rest);
-};

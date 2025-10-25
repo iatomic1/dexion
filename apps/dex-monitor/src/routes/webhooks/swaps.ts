f85e34215e303fff9b901c0d83f40fb9b138ce02
Atomic <atomic.k.2739@gmail.com>
2025-10-25 15:12:05 +0100

    ♻️ Refactor(dex-monitor): extract shared handleSwapWebhook function

--- apps/dex-monitor/src/routes/webhooks/swaps.ts
+++ apps/dex-monitor/src/routes/webhooks/swaps.ts
@@ -33,36 +33,8 @@ export async function handleSwapWebhook(c: Context) {
 	}
 }
 
-swaps.post("/velar", async (c) => {
-	try {
-		logger.debug("step1: got req");
-		const body = await c.req.json();
-		logger.debug({ body }, "step2: parsed body");
+swaps.post("/velar", handleSwapWebhook);
 
-		const txMetadata = body.event.apply[0].transactions[0].metadata;
-		logger.debug({ txMetadata }, "step3: tx meta");
-
-		const postConditions = txMetadata.post_conditions.post_conditions;
-		logger.debug({ postConditions }, "step4: post conds");
-
-		const assetContracts = extractAssetContracts(postConditions);
-		logger.debug({ assetContracts }, "step5: assets");
-
-		const senderAddress = txMetadata.sender_address;
-		logger.debug("step6: enqueue maybe");
-
-		if (txMetadata.status === "success")
-			await swapQueue.add("swap-event", { senderAddress, assetContracts });
-
-		return c.json({ message: "ok" }, 200);
-	} catch (err) {
-		logger.error(err, "velar webhook err:");
-		return c.json({ error: "fail" }, 500);
-	}
-});
-
-swaps.post("/bitflow", async (c) => {
-	handleSwapWebhook(c);
-});
+swaps.post("/bitflow", handleSwapWebhook);
 
 export default swaps;

92615d967306a3c44e45707a286ebcfac99a9cac
Atomic <atomic.k.2739@gmail.com>
2025-10-31 00:37:40 +0100

    ♻️ Refactor(packages): remove renamed packages

--- packages/token-watcher/src/services/stacks.ts
+++
@@ -1,6 +0,0 @@
-import { StacksApiSocketClient } from "@stacks/blockchain-api-client";
-
-// for testnet, replace with https://api.testnet.hiro.so
-const socketUrl = "https://api.mainnet.hiro.so";
-
-export const sc = new StacksApiSocketClient({ url: socketUrl });

7ffc1c94554e4548516f26a9f7071d5d08ec739d
Atomic <atomic.k.2739@gmail.com>
2025-11-03 06:58:27 +0100

    🔇 Refactor(backend): remove unnecessary logs

--- apps/backend/api/http/router/docs.go
+++ apps/backend/api/http/router/docs.go
@@ -15,14 +15,8 @@ func RegisterDocsRoutes(router *gin.RouterGroup) {
 	specUrl := filepath.Join(projectpath.Root, "/internal/docs/openapi.json")
 
 	log.Info().Str("specUrl", specUrl).Msg("Serving spec from")
-	docsGroup.GET("/test", func(c *gin.Context) {
-		c.String(http.StatusOK, "Docs group is working")
-	})
-	// spec2Url := filepath.Join(projectpath.Root, "/oas.json")
 
-	log.Info().Msg("routing")
 	docsGroup.GET("/reference", func(c *gin.Context) {
-		log.Info().Msg("httping")
 		content, err := scalargo.NewV2(
 			scalargo.WithSpecURL("/api/v1/docs/swagger.json"),
 			scalargo.WithMetaDataOpts(
@@ -30,7 +24,6 @@ func RegisterDocsRoutes(router *gin.RouterGroup) {
 			),
 			scalargo.WithTheme(scalargo.ThemeDeepSpace),
 			scalargo.WithLayout(scalargo.LayoutModern),
-			// scalargo.WithBaseServerURL("http://localhost:2020/"),
 		)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})

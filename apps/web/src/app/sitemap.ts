9d3acc6cd3d57d9a6810bab2aa6c2d2583a8fad5
Atomic <atomic.k.2739@gmail.com>
2025-11-06 02:06:20 +0100

    ♻️ Reafctor(web): use FRONTEND_URL for consistency

--- apps/web/src/app/sitemap.ts
+++ apps/web/src/app/sitemap.ts
@@ -4,7 +4,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
 	return [
 		{
-			url: FRONTEND_URL + "/",
+			url: `${FRONTEND_URL}/`,
 			lastModified: new Date("2025-10-01"),
 			changeFrequency: "weekly",
 			priority: 1.0,
@@ -16,19 +16,19 @@ export default function sitemap(): MetadataRoute.Sitemap {
 			priority: 0.8,
 		},
 		{
-			url: FRONTEND_URL + "/signup",
+			url: `${FRONTEND_URL}/signup`,
 			lastModified: new Date("2025-10-01"),
 			changeFrequency: "monthly",
 			priority: 0.8,
 		},
 		{
-			url: FRONTEND_URL + "/reset",
+			url: `${FRONTEND_URL}/reset`,
 			lastModified: new Date("2025-10-01"),
 			changeFrequency: "monthly",
 			priority: 0.5,
 		},
 		{
-			url: FRONTEND_URL + "/recover-account",
+			url: `${FRONTEND_URL}/recover-account`,
 			lastModified: new Date("2025-10-01"),
 			changeFrequency: "monthly",
 			priority: 0.5,

0dc4e0db79843586fa14d652013d734603c31a94
Atomic <atomic.k.2739@gmail.com>
2025-11-08 14:34:28 +0100

    🩹 Fix(web): use ExternalLink component

--- apps/web/src/config/site.ts
+++ apps/web/src/config/site.ts
@@ -1,10 +1,8 @@
+import { SOCIALS } from "@dexion/shared";
+
 const siteConfig = {
 	title: "Dexion",
-	socials: {
-		x: "https://x.com/dexion_pro",
-		telegram: "https://t.me/+JcgYNb9t4w45ZTA8",
-		docs: "https://docs.dexion.pro/",
-	},
+	socials: SOCIALS,
 	features: {
 		walletTracking: false,
 		priceAlerts: true,

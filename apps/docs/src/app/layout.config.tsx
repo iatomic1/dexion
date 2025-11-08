74e2f20cecb83da46946de191309d3baafb2842d
Atomic <atomic.k.2739@gmail.com>
2025-11-08 13:53:14 +0100

    📝 Feat(docs): add banner and update logo

--- apps/docs/src/app/layout.config.tsx
+++ apps/docs/src/app/layout.config.tsx
@@ -1,4 +1,5 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import Image from "next/image";
 
 /**
  * Shared layout configurations
@@ -8,21 +9,21 @@ import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
  * Docs Layout: app/docs/layout.tsx
  */
 export const baseOptions: BaseLayoutProps = {
-  nav: {
-    title: (
-      <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
-        Dexion
-      </>
-    ),
-  },
-  // see https://fumadocs.dev/docs/ui/navigation/links
-  links: [],
+	nav: {
+		title: (
+			<>
+				<Image
+					src="/logo.png"
+					alt="Company Logo"
+					width={24}
+					height={24}
+					// className="h-auto w-auto"
+					priority
+				/>
+				Dexion
+			</>
+		),
+	},
+	// see https://fumadocs.dev/docs/ui/navigation/links
+	links: [],
 };

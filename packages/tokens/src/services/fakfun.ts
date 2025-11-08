c3b603994fe4b8b1568f97c76cedb0cf59cb425f
Atomic <atomic.k.2739@gmail.com>
2025-11-08 13:47:47 +0100

    ✨ Feat(tokens): add support for fak.fun tokens

---
+++ packages/tokens/src/services/fakfun.ts
@@ -0,0 +1,47 @@
+import { FAKFUN_API_BASE_URL, FAKFUN_BEARER_TOKEN } from "@dexion/shared";
+import axios from "axios";
+import { transformFakFunToTokenMetadata } from "../utils";
+
+export async function getFakFunTokenMetadata(ca: string) {
+	try {
+		const url = `${FAKFUN_API_BASE_URL}tokens/${ca}`;
+		const { data } = await axios.get(url, {
+			headers: {
+				Authorization: `Bearer ${FAKFUN_BEARER_TOKEN}`,
+			},
+		});
+		const rawToken = data.data;
+
+		return transformFakFunToTokenMetadata(rawToken, "fakfun") || null;
+	} catch (err) {
+		console.error(err);
+		return null;
+	}
+}
+
+export async function getFakFunTrades(contractId: string) {
+	try {
+		const url = `${FAKFUN_API_BASE_URL}tokens/trades/${contractId}`;
+		const { data } = await axios.get(url, {
+			headers: {
+				Authorization: `Bearer ${FAKFUN_BEARER_TOKEN}`,
+			},
+		});
+
+		return data;
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+export async function fetchFakFunTokens() {
+	const response = await axios.get(
+		`${FAKFUN_API_BASE_URL}tokens?verified=true&search=&sortOrder=all&page=1&limit=1000`,
+		{
+			headers: {
+				Authorization: `Bearer ${FAKFUN_BEARER_TOKEN}`,
+			},
+		},
+	);
+	return response.data.results;
+}

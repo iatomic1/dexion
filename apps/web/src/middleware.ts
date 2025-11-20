8191306c624049d793672a56e44c4f7fd590c023
Atomic <atomic.k.2739@gmail.com>
2025-11-20 02:20:21 +0100

    ✨ Feat(web): only allow authorized emails

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,7 +1,7 @@
 import { getSessionCookie } from "better-auth/cookies";
 import { type NextRequest, NextResponse } from "next/server";
 
-const guestOnlyRoutes = ["/login", "/signup", "/recover-account"];
+const guestOnlyRoutes = ["/login", "/signup", "/recover-account", "/reset"];
 
 export async function middleware(request: NextRequest) {
 	const sessionCookie = getSessionCookie(request);

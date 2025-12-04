e86436752d69c3f538ca2df5b3db3b3999121c10
Atomic <atomic.k.2739@gmail.com>
2025-12-04 22:01:26 +0100

    🚑️ Fix(): update react and nextjs for urgent hotfix

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -7,10 +7,17 @@ const nextConfig = {
 	images: {
 		remotePatterns: [
 			{
+				protocol: "https",
 				hostname: "images.ctfassets.net",
 			},
 			{
+				protocol: "https",
 				hostname: "assets.hiro.so",
+			},
+			{
+				protocol: "https",
+				hostname: "szigdtxfspmofhxoytra.supabase.co",
+				pathname: "/storage/v1/object/public/token_logo/**",
 			},
 		],
 	},

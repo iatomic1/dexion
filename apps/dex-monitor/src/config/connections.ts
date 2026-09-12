47d625fe03274ed3e551aa8f446694fcd356544c
Atomic <atomic.k.2739@gmail.com>
2026-09-12 01:44:23 +0100

    🔊 Logs(dex-monitor): add redis logs to track latency

--- apps/dex-monitor/src/config/connections.ts
+++ apps/dex-monitor/src/config/connections.ts
@@ -8,3 +8,15 @@ export const bullMqRedisConnection = new Redis(config.BULLMQ_REDIS_URL, {
 export const normalRedisConnection = new Redis(config.REDIS_URL, {
 	maxRetriesPerRequest: null,
 });
+
+normalRedisConnection.on("connect", () => {
+	console.log("Redis connected");
+});
+
+normalRedisConnection.on("ready", () => {
+	console.log("Redis ready");
+});
+
+normalRedisConnection.on("error", (err) => {
+	console.error("Redis error", err);
+});
